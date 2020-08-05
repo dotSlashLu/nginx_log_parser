05cb0d6b44595cdb85ff677ca622b043a6bb0ff1
dotSlashLu <dotslash.lu@gmail.com>
2020-08-05 19:52:56 +0800

    perf: use str slice

--- src/lib.rs
+++ src/lib.rs
@@ -42,87 +42,88 @@ impl fmt::Display for TokErr {
 impl std::error::Error for TokErr {}
 
 impl Tokenizer {
-    // 113.106.106.3 - http [04/Aug/2020:14:18:07 +0800] "GET /[%20%20%20%20%20%7B%20%20%20%20%20%20%20%20%20%22placement%22:%22com.mopub.nativeads.WpsEventNative%22,%20%20%20%20%20%20%20%20%20%22plugin%22:%22ad_wps%22,%20%20%20%20%20%20%20%20%20%22ad_type%22:%222%22,%20%20%20%20%20%20%20%20%20%22show_confirm_dialog%22:%222%22,%20%20%20%20%20%20%20%20%20%22logo_gravity%22:%22left_top%22%20%20%20%20%20%20%20%20%20%7D] HTTP/1.1" 404 857 "http://infostream-adm.wps.kingsoft.net/edit_card?type=edit&id=597&resourceId=1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0" "-" "infostream-adm.wps.kingsoft.net" "172.16.49.100:30755" "-" 0.075 0.075
-    pub fn tok(
+    pub fn tok<'a>(
         self: &Self,
-        input: String,
-    ) -> Result<std::collections::HashMap<String, String>, TokErr> {
-        println!("input: {}", input);
-        let input = input.as_bytes();
+        input: &'a str,
+    ) -> Result<std::collections::HashMap<String, &'a str>, TokErr> {
+        dbg!(input);
         let ninput = input.len();
         let mut part_i = 0;
         let mut input_i = 0;
         let nparts = self.fields.len();
-        let mut res = std::collections::HashMap::<String, String>::new();
+        let mut res = std::collections::HashMap::<String, &'a str>::new();
+        // remove first str
+        if let CfgPart::Str { value } = &self.fields[part_i] {
+            input_i += value.len();
+            part_i += 1;
+        }
         loop {
-            match &self.fields[part_i] {
-                CfgPart::Str { value } => {
-                    println!("a str!!!, len {}", value.len());
-                    input_i += value.len();
-                    println!("input_i: {}", input_i);
-                    part_i += 1;
+            if let CfgPart::Variable { name } = &self.fields[part_i] {
+                println!("a new var!!!");
+                // last part is a variable
+                if part_i + 1 == nparts {
+                    dbg!("var at last");
+                    let value = &input[input_i..];
+                    res.insert(name.clone(), value);
+                    break;
                 }
-                CfgPart::Variable { name } => {
-                    println!("a variable!!!");
-                    // last variable
-                    if part_i + 1 == nparts {
-                        println!("var at last");
-                        let value = String::from_utf8(Vec::from(&input[input_i..]))?;
-                        res.insert(name.clone(), value);
+                // read variable ending str
+                let next_str = &self.fields[part_i + 1];
+                let end_bytes = match next_str {
+                    CfgPart::Str { value } => {
+                        println!("str part {} of {}", part_i, nparts);
                         part_i += 1;
+                        value
+                    }
+                    _ => {
+                        return Err(TokErr {
+                            reason: String::from("wrong sequence"),
+                        })
+                    }
+                };
+                let end_bytes_len = end_bytes.len();
+                let start_i = input_i;
+                let mut end_i = input_i;
+                loop {
+                    let vlen = end_i - start_i;
+                    if vlen >= end_bytes_len
+                    // && &input[start_i + vlen - end_bytes_len..start_i + vlen] == end_bytes
+                    {
+                        if &input[start_i + vlen - end_bytes_len..start_i + vlen] == end_bytes {
+                            println!("before subtracting, end_i: {}", end_i);
+                            end_i = end_i - end_bytes_len;
+                            println!("end_i: {}", end_i);
+                            break;
+                        }
+                        println!(
+                            "var tail: {}, str: {}",
+                            &input[start_i + vlen - end_bytes_len..start_i + vlen],
+                            end_bytes
+                        );
+                    }
+                    input_i += 1;
+                    end_i += 1;
+                    // EOL
+                    //  since we use slicing op and slicing op has an open upper bound
+                    //  so input_i is allowed to be equal to ninput
+                    if input_i == ninput + 1 {
                         break;
                     }
-                    let next_str = &self.fields[part_i + 1];
-                    let end_bytes = match next_str {
-                        CfgPart::Str { value } => {
-                            part_i += 1;
-                            println!("part {} of {}", part_i, nparts);
-                            value.as_bytes()
-                        }
-                        _ => {
-                            return Err(TokErr {
-                                reason: String::from("wrong sequence"),
-                            })
-                        }
-                    };
-                    let end_bytes_len = end_bytes.len();
-                    let mut value = Vec::new();
-                    loop {
-                        let vlen = value.len();
-                        if vlen > end_bytes_len - 1
-                            && &value[vlen - end_bytes_len..vlen] == end_bytes
-                        {
-                            value.truncate(vlen - end_bytes_len);
-                            break;
-                        }
-                        // if vlen > end_bytes_len - 1 {
-                        //     if &value[vlen - end_bytes_len..vlen] == end_bytes {
-                        //         value.truncate(vlen - end_bytes_len);
-                        //         break;
-                        //     } else {
-                        //         // println!(
-                        //         //     "{:?} neq to end bytes {:?}({})",
-                        //         //     &value[vlen - 1 - end_bytes_len..vlen - 1],
-                        //         //     end_bytes,
-                        //         //     String::from_utf8(end_bytes.to_vec())?
-                        //         // );
-                        //     }
-                        // }
-                        value.push(input[input_i]);
-                        input_i += 1;
-                        if input_i == ninput - 1 {
-                            break;
-                        }
-                    }
-                    let value = String::from_utf8(value)?;
-                    res.insert(name.clone(), value);
-                    part_i += 1;
-                    println!("part {} of {}", part_i, nparts);
                 }
+                let value = &input[start_i..end_i];
+                println!("variable part {} of {}", part_i, nparts);
+                dbg!(value);
+                res.insert(name.clone(), value);
+                part_i += 1;
             }
-            if part_i > nparts - 1 || input_i >= ninput - 1 {
-                println!("{}", part_i >= nparts - 1);
-                break;
+            if part_i == nparts {
+                return Ok(res)
+            }
+            if input_i > ninput + 1{
+                println!("parts {} input_i {}", part_i >= nparts, input_i > ninput + 1);
+                return Err(TokErr {
+                    reason: "boundary check failed, field mismatch?".to_owned(),
+                });
             }
         }
         Ok(res)
@@ -141,9 +142,7 @@ fn parse_cfg_str(field_cfg_str: String) -> Vec<CfgPart> {
     let mut rest = field_cfg_str;
     loop {
         let (optional_part, rest_tmp) = parse_cfg_str_part(rest);
-        println!("rest: {:?}", rest_tmp);
         if let Some(part) = optional_part {
-            // println!("got part: {:?}", part);
             res.push(part);
             rest = String::from(rest_tmp);
             continue;
@@ -208,14 +207,14 @@ mod tests {
     #[test]
     fn test_tokenizer() {
         use super::*;
-        let test_str: [&str; 1] = [
-            //r#"$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$gzip_ratio""#,
-            r#"$remote_addr - $scheme [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for" "$host" "$upstream_addr" "$upstream_cache_status" $request_time $upstream_response_time"#,
+        let test_str: [&str; 2] = [
+            r#"123$remote_addr - $scheme [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for" "$host" "$upstream_addr" "$upstream_cache_status" $request_time $upstream_response_time"#,
+            r#"$remote_addr - $scheme [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for" "$host" "$upstream_addr" "$upstream_cache_status" $request_time $upstream_response_time zzz"#,
         ];
         for &cfg in test_str.iter() {
             let tokenizer = new(cfg.to_owned());
             println!("{:?}", tokenizer);
-            let res = tokenizer.tok(r#"113.106.106.3 - http [04/Aug/2020:14:18:07 +0800] "GET /[%20%20%20%20%20%7B%20%20%20%20%20%20%20%20%20%22placement%22:%22com.mopub.nativeads.WpsEventNative%22,%20%20%20%20%20%20%20%20%20%22plugin%22:%22ad_wps%22,%20%20%20%20%20%20%20%20%20%22ad_type%22:%222%22,%20%20%20%20%20%20%20%20%20%22show_confirm_dialog%22:%222%22,%20%20%20%20%20%20%20%20%20%22logo_gravity%22:%22left_top%22%20%20%20%20%20%20%20%20%20%7D] HTTP/1.1" 404 857 "http://infostream-adm.wps.kingsoft.net/edit_card?type=edit&id=597&resourceId=1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0" "-" "infostream-adm.wps.kingsoft.net" "172.16.49.100:30755" "-" 0.075 0.075"#.to_owned());
+            let res = tokenizer.tok(r#"123113.106.106.3 - http [04/Aug/2020:14:18:07 +0800] "GET /[%20%20%20%20%20%7B%20%20%20%20%20%20%20%20%20%22placement%22:%22com.mopub.nativeads.WpsEventNative%22,%20%20%20%20%20%20%20%20%20%22plugin%22:%22ad_wps%22,%20%20%20%20%20%20%20%20%20%22ad_type%22:%222%22,%20%20%20%20%20%20%20%20%20%22show_confirm_dialog%22:%222%22,%20%20%20%20%20%20%20%20%20%22logo_gravity%22:%22left_top%22%20%20%20%20%20%20%20%20%20%7D] HTTP/1.1" 404 857 "http://infostream-adm.wps.kingsoft.net/edit_card?type=edit&id=597&resourceId=1" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0" "-" "infostream-adm.wps.kingsoft.net" "172.16.49.100:30755" "-" 0.075 0.075"#);
             println!("{:?}", res);
         }
     }
